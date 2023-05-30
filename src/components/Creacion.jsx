d466b3d282e4e097a969b99474d28e24fe754c12
jorge <georgee.387@gmail.com>
2023-05-29 20:55:55 -0500

    eleminando archivos y subiendo el server

---
+++ src/components/Creacion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Creacion = () => {
+  return (
+    <div> CREACION DE MANILLAS</div>
+  )
+}
+
+export default Creacion
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Creacion from './components/Creacion';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Creacion />
   </React.StrictMode>
 );
 
